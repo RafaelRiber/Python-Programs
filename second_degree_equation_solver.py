59fbc022e045d49c1cfbe90b25825812df10fcf9
V3sth4cks153 <V3sth4cks153@users.noreply.github.com>
2015-09-13 17:01:09 +0200

    Update second_degree_equation_solver.py
    Moved float conversion to input, thus removing the need of three variables (af, bf and cf).

--- second_degree_equation_solver.py
+++ second_degree_equation_solver.py
@@ -8,45 +8,41 @@ print "|   __| . | | | .'|  _| | . |   |  |__   | . | | | | -_|  _|  | | |_ _| |
 print "|_____|_  |___|__,|_| |_|___|_|_|  |_____|___|_|\_/|___|_|     \_/|_|_____|"
 print "        |_|                                                                "
 
-print "\nWelcome in the Equation Resolver by V3sth4cks153. Please give the values for 'a', 'b' and 'c' as follows: ax^2+bx+c = 0.\n"
+print "\nWelcome in the Equation Resolver. Please give the values for 'a', 'b' and 'c' as follows: ax^2+bx+c = 0.\n"
 
-a = input("Value of 'A': ")
+a = float(input("Value of 'A': "))
 
 if a != 0:
 	print "A = %s" % (a)
 else:
 	sys.exit("Invalid value. Please enter only numbers other than zero.")
 
-b = input("Value of 'B': ")
+b = float(input("Value of 'B': "))
 
 if b != 0:
 	print "B = %s" % (b)
 else:
 	sys.exit("Invalid value. Please enter only numbers other than zero.")
 
-c = input("Value of 'C': ")
+c = float(input("Value of 'C': "))
 
 if c != 0:
 	print "C = %s" % (c)
 else:
 	sys.exit("Invalid value. Please enter only numbers other than zero.")
 
-af = float(a)
-bf = float(b)
-cf = float(c)
-
-discriminant = (bf * bf) - 4 * (af * cf)
+discriminant = (b * b) - 4 * (a * c)
 
 if discriminant >= 0:
 	print "\nThe discriminant is equal to: %s.\n" % (discriminant)
 else:
 	sys.exit("The equation is unresolvable. The discriminant is negative.")
 
-x1 = (-bf - math.sqrt(discriminant) ) / (2 * af)
-x2 = (-bf + math.sqrt(discriminant) ) / (2 * af)
-x3 = (-bf) / (2 * af)
+x1 = (-b - math.sqrt(discriminant) ) / (2 * a)
+x2 = (-b + math.sqrt(discriminant) ) / (2 * a)
+x3 = (-b) / (2 * a)
 
 if discriminant == 0:
     print "Sole solution of the equation: %s" % (x3)
 else:
-	print "Solutions: (%s; %s) \n\nThank you for using the Equation Solver by V3sth4cks153 !" % (x1, x2)
+	print "Solutions: (%s; %s) \n\nThank you for using the Equation Solver !" % (x1, x2)
