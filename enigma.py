630ec095314fe17a842f68e85626155c96e1981b
V3sth4cks153 <rafael@riber.ch>
2015-09-15 22:22:50 +0200

    Implemented the use of functions to simplify the code, removing the score feature for now.

--- enigma.py
+++ enigma.py
@@ -7,6 +7,30 @@ from os import name
 def cls():
 	system(['clear','cls'][name == 'nt'])
 
+def go(ans):
+	if ans == "oui" or ans == "o":
+		print "Alors c'est parti !"
+	elif ans == "non" or ans == "n":
+		sys.exit("Pas de problème ! Revenez quand vous voulez !")
+	else:
+		sys.exit("Pas de problème ! Revenez quand vous voulez !")
+
+def ans_a(ans):
+	if ans == "A" or ans == "a":
+		print "Bonne réponse !"
+	elif ans == "B" or ans == "b":
+		exit("Mauvaise réponse ! Dommage !")
+	else:
+		exit("Réponse invalide.")
+
+def ans_b(ans):
+	if ans == "B" or ans == "b":
+		print "Bonne réponse !"
+	elif ans == "A" or ans == "a":
+		exit("Mauvaise réponse ! Dommage !")
+	else:
+		exit("Mauvaise réponse ! Dommage !")
+
 print "╔═╗╔╗╔╦╔═╗╔╦╗╔═╗    "
 print "║╣ ║║║║║ ╦║║║╠═╣    "
 print "╚═╝╝╚╝╩╚═╝╩ ╩╩ ╩    "
@@ -32,13 +56,7 @@ rawans = raw_input("Alors %s, prêt à jouer ? Répondre par 'oui' ou par 'non':
 cls()
 
 ans = rawans.lower()
-
-if ans == "oui" or ans == "o":
-	print "Alors c'est parti !"
-elif ans == "non" or ans == "n":
-	sys.exit("Pas de problème ! Revenez quand vous voulez !")
-else:
-	sys.exit("Pas de problème ! Revenez quand vous voulez !")
+go(ans)
 
 print "\nVoici le principe du jeu: vous allez être présenté avec plusieurs énigmes et questions. \nPour chacune, vous aurez deux réponses a choix. Toute réponse correcte vous donnera 5 points,\ntoute réponse fausse vous enlèvera un point."
 
@@ -47,19 +65,8 @@ rawans = raw_input("\nDébuter le jeu ? Répondre par 'oui' ou par 'non': ")
 ans = rawans.lower()
 
 cls()
-
-if ans == "oui" or ans == "o":
-	print "C'est parti !"
-elif ans == "non" or ans == "n":
-	sys.exit("Pas de problème ! Revenez quand vous voulez !")
-else:
-	sys.exit("Pas de problème ! Revenez quand vous voulez !")
-
-
-score = 0
-
+go(ans)
 #----------------------------------------------------------------------------
-
 print "\nPremière question:"
 print "David a 10 ans, son petit frère Franck a la moitié de son âge.\nQuand David sera 10 fois plus âgé, quel âge aura Franck ?"
 print "A: 95 ans"
@@ -68,17 +75,9 @@ print "B: 50 ans"
 ans = raw_input("\nQuelle est la bonne réponse ? Répondre 'A' ou 'B': ")
 
 cls()
-
-if ans == "A" or ans == "a":
-	print "Bonne réponse ! Vous gagnez 5 points !"; score = score + 5
-elif ans == "B" or ans == "b":
-	print "Mauvaise réponse ! Dommage ! Vous perdez 1 point !"; score = score - 1
-else:
-	print "Réponse invalide. Vous ne gagnez ni perdez aucun point."
-
+ans_a(ans)
 #----------------------------------------------------------------------------
-
-print "\nBien joué %s ! Votre score est maintenant de %s points. Allons la question suivante." % (nom, score)
+print "\nBien joué %s ! Allons la question suivante." % (nom)
 
 print "\nDeuxième question:"
 print "Si nous ne sommes pas le lendemain de lundi ou le jour avant jeudi, que demain n'est pas dimanche,\nque ce n'était pas dimanche hier et que le jour d'après-demain n'est pas samedi,\net que le jour avant hier n'était pas mercredi, quel jour sommes-nous ?"
@@ -88,17 +87,9 @@ print "B: Dimanche"
 ans = raw_input("\nQuelle est la bonne réponse ? Répondre 'A' ou 'B': ")
 
 cls()
-
-if ans == "B" or ans == "b":
-	print "Bonne réponse ! Vous gagnez 5 points !"; score = score + 5
-elif ans == "A" or ans == "a":
-	print "Mauvaise réponse ! Dommage ! Vous perdez 1 point !"; score = score - 1
-else:
-	print "Réponse invalide. Vous ne gagnez ni perdez aucun point."
-
+ans_b(ans)
 #----------------------------------------------------------------------------
-
-print "\nBien joué %s ! Votre score est maintenant de %s points. Allons la question suivante." % (nom, score)
+print "\nBien joué %s ! Allons la question suivante." % (nom)
 
 print "\nTroisième question:"
 print "Quel nombre divisé par lui-même donne son double ?"
@@ -108,17 +99,9 @@ print "B: 2.5"
 ans = raw_input("\nQuelle est la bonne réponse ? Répondre 'A' ou 'B': ")
 
 cls()
-
-if ans == "A" or ans == "a":
-	print "Bonne réponse ! Vous gagnez 5 points !"; score = score + 5
-elif ans == "B" or ans == "b":
-	print "Mauvaise réponse ! Dommage ! Vous perdez 1 point !"; score = score - 1
-else:
-	print "Réponse invalide. Vous ne gagnez ni perdez aucun point."
-
+ans_a(ans)
 #----------------------------------------------------------------------------
-
-print "\nBien joué %s ! Votre score est maintenant de %s points. Allons la question suivante." % (nom, score)
+print "\nBien joué %s ! Allons la question suivante." % (nom)
 
 print "\nQuatrième question:"
 print "Sur un arbre à 2 branches, des oiseaux se parlent, l'un d'en haut dit à ceux d'en bas 'Si un de vous nous rejoint, nous serons alors 2 fois plus que vous,\npar contre si un de nous descend, nous serons alors à égalité'. Combien y a-t-il d'oiseaux sur les branches ?"
@@ -128,31 +111,7 @@ print "B: 5 en haut et 2 en bas"
 ans = raw_input("\nQuelle est la bonne réponse ? Répondre 'A' ou 'B': ")
 
 cls()
-
-if ans == "A" or ans == "a":
-	print "Bonne réponse ! Vous gagnez 5 points !"; score = score + 5
-elif ans == "B" or ans == "b":
-	print "Mauvaise réponse ! Dommage ! Vous perdez 1 point !"; score = score - 1
-else:
-	print "Réponse invalide. Vous ne gagnez ni perdez aucun point."
-
-
-#----------------------------------------------------------------------------
+ans_a(ans)
 
 cls()
-
-if score >= 3:
-	print "Vous êtes arrivés au bout du jeu avec un score de %s points ! Felicitations ! Vous avez droit a un gâteau !" % (score);
-
-	print "                                ";
-	print "       _..--'''@   @'''--.._    ";
-	print "     .'   @_/-//-\/>/>'/ @  '.  ";
-	print "    (  @  /_<//<'/----------^-) ";
-	print "    |'._  @     //|###########| ";
-	print "    |~  ''--..@|',|}}}}}}}}}}}| ";
-	print "    |  ~   ~   |/ |###########| ";
-	print "    | ~~  ~   ~|./|{{{{{{{{{{{| ";
-	print "     '._ ~ ~ ~ |,/````````````` ";
-	print "       ''--.~.|/                ";
-else:
-	print "Vous n'avez pas obtenu assez de points pour la récompense ! Rejouez !"
+print "Vous êtes arrivés au bout du jeu! Félicitations !
