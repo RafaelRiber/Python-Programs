7835796119b802544ce8e43fadb68ee89f202ebb
V3sth4cks153 <rafael@riber.ch>
2015-09-15 19:42:40 +0200

    Version 2.0
    New title.
    Implemented the use of a check function to shorten the code by 8 lines.

--- second_degree_equation_solver.py
+++ second_degree_equation_solver.py
@@ -2,47 +2,38 @@
 from sys import exit
 from math import sqrt
 
-print " _____             _   _            _____     _                      ___   "
-print "|   __|___ _ _ ___| |_|_|___ ___   |   __|___| |_ _ ___ ___    _ _  |_  |  "
-print "|   __| . | | | .'|  _| | . |   |  |__   | . | | | | -_|  _|  | | |_ _| |_ "
-print "|_____|_  |___|__,|_| |_|___|_|_|  |_____|___|_|\_/|___|_|     \_/|_|_____|"
-print "        |_|                                                                "
+print "   ____               __  _             ____     __               _   __  ___ "
+print "  / __/__ ___ _____ _/ /_(_)__  ___    / __/__  / /  _____ ____  | | / / |_  |"
+print " / _// _ `/ // / _ `/ __/ / _ \/ _ \  _\ \/ _ \/ / |/ / -_) __/  | |/ / / __/ "
+print "/___/\_, /\_,_/\_,_/\__/_/\___/_//_/ /___/\___/_/|___/\__/_/     |___(_)____/ "
+print "      /_/                                                                     "
 
-print "\nWelcome in the Equation Solver by V3sth4cks153. Please give the values for 'a', 'b' and 'c' as follows: ax^2+bx+c = 0.\n"
+print "\nWelcome in the Equation Solver 2.0 by V3sth4cks153. Please give the values for 'a', 'b' and 'c' as follows: ax^2+bx+c = 0.\n"
+
+def check(x):
+    if x != 0:
+	    print "%s is a valid value. Please go on." % (x)
+    else:
+	    exit("Invalid value. Please enter only numbers other than zero.")
 
 a = float(input("Value of 'A': "))
-
-if a != 0:
-	print "A = %s" % (a)
-else:
-	exit("Invalid value. Please enter only numbers other than zero.")
-
+check(a)
 b = float(input("Value of 'B': "))
-
-if b != 0:
-	print "B = %s" % (b)
-else:
-	exit("Invalid value. Please enter only numbers other than zero.")
-
+check(b)
 c = float(input("Value of 'C': "))
+check(c)
 
-if c != 0:
-	print "C = %s" % (c)
-else:
-	exit("Invalid value. Please enter only numbers other than zero.")
-
-discriminant = (b * b) - 4 * (a * c)
-
-if discriminant >= 0:
-	print "\nThe discriminant is equal to: %s.\n" % (discriminant)
-else:
-	exit("The equation is unsolvable: The discriminant is negative.")
-
-x1 = (-b - sqrt(discriminant) ) / (2 * a)
-x2 = (-b + sqrt(discriminant) ) / (2 * a)
+dis = (b * b) - 4 * (a * c)
+x1 = (-b - sqrt(dis) ) / (2 * a)
+x2 = (-b + sqrt(dis) ) / (2 * a)
 x3 = (-b) / (2 * a)
 
-if discriminant == 0:
-    print "Sole solution of the equation: %s" % (x3)
+if dis >= 0:
+	print "\nThe discriminant is equal to: %s.\n" % (dis)
 else:
-	print "Solutions: (%s; %s) \n\nThank you for using the Equation Solver by V3sth4cks153 !" % (x1, x2)
+	exit("The equation has no real roots: The discriminant is negative.")
+
+if dis == 0:
+    print "Sole root of the equation: %s" % (x3)
+else:
+	print "Roots: (%s; %s) \n\nThank you for using the Equation Solver by V3sth4cks153 !" % (x1, x2)
